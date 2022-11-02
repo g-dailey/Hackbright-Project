b52d84f17803f000ec806afe791c390e4480324f
g-dailey <gulafroz.rezai@gmail.com>
2022-11-02 10:56:42 -0700

    Modified FormFields from StringFields to SelectField and MultipleSelectField

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField, SelectField, SelectMultipleField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_wtf.file import FileField, FileAllowed
 from model import User, Pairing, Meeting, Prompt, TimeSlot, Timezone, Feedback
@@ -12,13 +12,13 @@ class SignUpForm(FlaskForm):
   last_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=20)])
   password = PasswordField('Password', validators=[DataRequired()])
   #profile picture?
-  prompt_difficulty_level = StringField('Prompt Difficulty Level', validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
-  primary_language = StringField('Primary Language', validators=[DataRequired()]) #how to make this a single select field and still exist in this form?
-  programming_language = StringField('Programming Language', validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
-  timezone = StringField('Timezone', validators=[DataRequired()]) #how to make this a single select field and still exist in this form?
+  prompt_difficulty_level = SelectField('Prompt Difficulty Level', choices=[(1, 'Easy'), (2, 'Medium'), (3, 'Hard')], validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
+  primary_language = SelectField('Primary Language',choices=[('eng', 'English'), ('sp', 'Spanish'), ('fr', 'French'), ('fa', 'Farsi'), ('ge', 'German')], validators=[DataRequired()]) #how to make this a single select field and still exist in this form?
+  programming_language = SelectField('Programming Language', choices=[('cpp', 'C++'), ('py', 'Python'), ('js', 'JavaScript'), ('ja', 'Java'), ('text', 'Plain Text')], validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
+  timezone = SelectField('Timezone', choices=[(1, 'Pacific Time Zone'), (2, 'Central Time Zone'), (3, 'Eastern Time Zone'), (4, 'Alaska Time Zone')], validators=[DataRequired()]) #how to make this a single select field and still exist in this form?
   #how to make it easy to select slots from day of the week and timeslots quickly rather than manualy entry for every day of the week?
-  day_of_week = StringField('Time Slots', validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
-  timeslots = StringField('Time Slots', validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
+  day_of_week = SelectMultipleField('Day of the Week', choices=[(1, 'Sunday'), (2, 'Monday'), (3, 'Tuesday'), (4, 'Wednesday'), (5, 'Thursday'), (6, 'Friday'), (7, 'Saturday')], validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
+  timeslots = SelectMultipleField('Time Slots', choices=[(1, '7am - 8am'), (2, '8am - 9am'), (3, '9am - 10am'), (4, '10am - 11am'), (5, '11am - 12pm'), (6, '12pm - 1pm'), (7, '1pm - 2pm'), (8, '2pm - 3pm'), (9, '3pm - 4pm'), (10, '4pm - 5pm'), (11, '5pm - 6pm'), (12, '6pm - 7pm'), (13, '8pm - 9pm'), (14, '9pm - 10pm'),(15, '10pm - 11pm'),(16, '11pm - 12am')], validators=[DataRequired()]) #how to make this a multiple select field and still exist in this form?
   submit = SubmitField('Sign Up')
 
   def validate_email(self, email):
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from model import User, Pairing, Meeting, Prompt, TimeSlot, Timezone, Feedback
 import jinja2
 from forms import SignUpForm, LoginForm, UpdateAccountForm
+from model import db
 import email_validator
 
 app = Flask(__name__)
@@ -16,20 +17,28 @@ app = Flask(__name__)
 app.secret_key = "DEV"
 
 @app.route("/", methods=['GET', 'POST'])
-
 def home():
   return render_template("homepage.html")
 
 
 @app.route("/register", methods=['GET', 'POST'])
-
 def register():
   form = SignUpForm()
-  name = session.get("name")
-  if request.method == 'POST':
-      name = session.get("name")
-      session['name'] = name
-      return f"<h1> {name} </h1>"
+
+  if form.validate_on_submit():
+    user = User(email=form.email.data,
+                password=form.password.data,
+                first_name=form.first_name.data,
+                last_name=form.last_name.data,
+                prompt_difficulty_level=form.prompt_difficulty_level.data,
+                primary_language=form.primary_language.data,
+                programming_language=form.programming_language.data,
+                timezone=form.timezone.data,
+                day_of_week=form.day_of_week.data,
+                timeslots=form.timeslots.data)
+    db.session.add(user)
+    db.session.commit()
+    return redirect('thank-you.html', form=form)
   return render_template("register.html", form=form)
 
 
