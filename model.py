e7d8ff6f879b931c89686ed66a923aa701507078
g-dailey <gulafroz.rezai@gmail.com>
2022-11-01 22:02:26 -0700

    Completed the first draft of data model and created the classes

--- model.py
+++ model.py
@@ -1,4 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 db = SQLAlchemy()
 
@@ -15,26 +18,132 @@ class User(db.Model):
     last_name = db.Column(db.String)
     email = db.Column(db.String, unique= True)
     password = db.Column(db.String)
-    feedback = db.relationship("Feedback", back_populates="user")
+    profile_picture = db.Column(db.String)
+    prompt_difficulty_level = db.Column(db.String)
+    primary_language_id = db.Column(db.Integer, db.ForeignKey("primary_languages.primary_language_id"))
+    timezone_id = db.Column(db.Integer, db.ForeignKey("timezones.timezone_id"))
+
+
+    # feedback = db.relationship("Feedback", back_populates="user")
 
     def __repr__(self):
         return f'<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name} email={self.email}>'
+
+class TimeSlot(db.Model):
+    __tablename__ = "timeslots"
+
+    timeslot_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    timeslot_name = db.Column(db.String)
+    day_of_the_week = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<TimeSlot timeslot_id={self.timeslot_id} timeslot_name={self.timeslot_name}>'
+
+
+class UserTimeSlotMapping(db.Model):
+    __tablename__ = "users_timeslot_mapping"
+
+    user_timeslot_mapping_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    timeslot_id = db.Column(db.Integer, db.ForeignKey("timeslots.timeslot_id"))
+
+
+    def __repr__(self):
+        return f'<User Timeslot Mapping user_timeslot_mapping_id={self.user_timeslot_mapping_id} timeslot_id={self.timeslot_id}>'
+
+class PrimaryLanguage(db.Model):
+    __tablename__ = "primary_languages"
+
+    primary_language_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    primary_language_name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Primary Language primary_language_id={self.primary_language_id} primary_language_name={self.primary_language_name}>'
+
+
+class ProgrammingLanguage(db.Model):
+    __tablename__ = "programming_languages"
+
+    programming_language_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    programming_language_name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Programming Language programming_language_id={self.programming_language_id} programming_language_name={self.programming_language_name}>'
+
+class UserProgrammingLanguageMapping(db.Model):
+    __tablename__ = "users_programming_language_mapping"
+
+    user_programming_language_mapping_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    programming_language_id = db.Column(db.Integer, db.ForeignKey("programming_languages.programming_language_id"))
+
+
+    def __repr__(self):
+        return f'<Programming Language Mapping user_programming_language_mapping_id={self.user_programming_language_mapping_id} programming_language_id={self.programming_language_id}>'
+
+
+class Timezone(db.Model):
+    __tablename__ = "timezones"
+
+    timezone_id = db.Column(db.Integer,
+                        autoincrement= True,
+                        primary_key= True)
+    timezone_name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Programming Language timezone_id={self.timezone_id} timezone_name={self.timezone_name}>'
+
 
 class Feedback(db.Model):
     """Feedback"""
 
     __tablename__ = "feedback"
+
     feedback_id = db.Column(db.Integer, autoincrement = True, primary_key= True)
     score = db.Column(db.Integer)
-    prompt_id = db.Column(db.Integer, db.ForeignKey("prompts.prompt_id"))
-    feedback_for = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
-
-    prompt = db.relationship("Prompt", back_populates="feedback")
-    user = db.relationship("User", back_populates="feedback")
+    feedback_description = db.Column(db.Text)
+    is_prompt_solved = db.Column(db.Boolean)
+    pairing_id = db.Column(db.Integer, db.ForeignKey("pairings.pairing_id"))
+    sender_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    recipient_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
 
     def __repr__(self):
-            return f"<Feedback feedback_id={self.feedback_id} feedback_for={self.feedback_for} score={self.score}>"
+            return f"<Feedback feedback_id={self.feedback_id} feedback_description={self.feedback_description} is_prompt_solved={self.is_prompt_solved}>"
+
+
+class Pairing(db.Model):
+    """Pairing"""
+
+    __tablename__ = "pairings"
+
+    pairing_id = db.Column(db.Integer, autoincrement = True, primary_key= True)
+    meeting_id = db.Column(db.Integer, db.ForeignKey("meetings.meeting_id"))
+    user_one_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    user_two_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+
+    def __repr__(self):
+            return f"<Pairing pairing_id={self.pairing_id} >" #am I am able to print the foreign key values here?
+
+
+class Meeting(db.Model):
+    """Meeting"""
+
+    __tablename__ = "meetings"
+
+    meeting_id = db.Column(db.Integer, autoincrement = True, primary_key= True)
+    meeting_date = db.Column(db.String) #is there a datetime option?
+
+    def __repr__(self):
+            return f"<Meeting meeting_id={self.meeting_id} meeting_date={self.meeting_date}>"
+
 
 class Prompt(db.Model):
     """Prompt"""
@@ -43,18 +152,16 @@ class Prompt(db.Model):
 
     prompt_id = db.Column(db.Integer, autoincrement = True, primary_key= True)
     prompt_name = db.Column(db.String)
-    feedback_id = db.Column(db.Integer, db.ForeignKey("feedback.feedback_id"))
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
+    prompt_link = db.Column(db.String)
 
-    prompt = db.relationship("Prompt", back_populates="feedback")
-    user = db.relationship("User", back_populates="feedback")
+    # prompt = db.relationship("Prompt", back_populates="feedback")
+    # user = db.relationship("User", back_populates="feedback")
 
     def __repr__(self):
-            return f"<Feedback feedback_id={self.feedback_id} feedback_for={self.feedback_for} score={self.score}>"
+            return f"<Prompt prompt_id={self.prompt_id} prompt_name={self.prompt_name} prompt_link={self.prompt_link}>"
 
 
-
-def connect_to_db(flask_app, db_uri="postgresql:///prompts", echo=True):
+def connect_to_db(flask_app, db_uri="postgresql:///users", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -64,6 +171,9 @@ def connect_to_db(flask_app, db_uri="postgresql:///prompts", echo=True):
 
     print("You have been connected to the db")
 
+class NamerForm(FlaskForm):
+    name = StringField("What's your name")
+    submit = SubmitField("Submit")
 
 if __name__ == "__main__":
     from server import app
--- server.py
+++ server.py
@@ -1,29 +1,47 @@
-from flask import Flask
+
 from flask import Flask, render_template, redirect, flash, session, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 import jinja2
 
 app = Flask(__name__)
 
-@app.route("/")
+
+app.secret_key = "DEV"
+
+@app.route("/", methods=['GET', 'POST'])
 
 def home():
   return render_template("homepage.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 
 def register():
+  name = session.get("name")
+  if request.method == 'POST':
+      name = session.get("name")
+      session['name'] = name
+      return f"<h1> {name} </h1>"
   return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/form-signup", methods=['GET', 'POST'])
+def get_name():
+    name = request.form['name']
+    session['name'] = name
+    return redirect('register.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
 
 def login():
   return render_template("login.html")
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
