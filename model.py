d495199214671e10f7a9f53fab14a9b465b4c064
g-dailey <gulafroz.rezai@gmail.com>
2023-01-14 14:06:56 -0800

    Populated Comments Table with data

--- model.py
+++ model.py
@@ -4,10 +4,13 @@ from sqlalchemy.orm import relationship, Session
 from flask_bcrypt import Bcrypt
 import pandas as pd
 import csv
-
 from flask_login import LoginManager
 import enum
 from sqlalchemy import Integer, Enum
+import os
+from os import listdir
+from random import choice
+import random
 
 
 
@@ -24,12 +27,7 @@ def connect_to_db(flask_app, db_uri="postgresql:///coder-lounge", echo=True):
     bcrypt = Bcrypt(flask_app)
     # login_manager = LoginManager(flask_app)
     db.app = flask_app
-
-
     db.init_app(flask_app)
-
-
-
     print("You have been connected to the db")
 
 
@@ -48,19 +46,13 @@ class User(db.Model):
     last_name = db.Column(db.String)
     email = db.Column(db.String, unique= True)
     password = db.Column(db.String)
-    profile_picture = db.Column(db.String, default='default-image.jpeg')
+    profile_picture = db.Column(db.String, default="default-image.jpeg")
     prompt_difficulty_level = db.Column(db.String)
     primary_language = db.Column(db.String)
     timezone_name = db.Column(db.String)
     programming_languages = relationship('ProgrammingLanguage', secondary='users_programming_language_mapping', back_populates='users')
     selected_timeslots = relationship('TimeSlot', secondary='users_timeslot_mapping', back_populates='users_timeslots')
     prompts_assigned = relationship('Prompt', back_populates='user_prompts')
-    #User.query.get(7).__dict__
-    #_[0].__dict__
-
-    # def get_reset_token(self, expires_sec=1800):
-    #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
-    #     return s.dumps({'user_id': self.user_id}).decode('utf-8')
 
     def __repr__(self):
         return f'<User user_id={self.user_id} first_name={self.first_name} last_name={self.last_name} email={self.email}>'
@@ -153,6 +145,9 @@ class Comment(db.Model):
     def __repr__(self):
         return '<Post %r>' % (self.body)
 
+
+
+
 class Prompt(db.Model):
     """Prompt"""
 
@@ -162,6 +157,7 @@ class Prompt(db.Model):
     prompt_name = db.Column(db.String)
     prompt_link = db.Column(db.String)
     prompt_difficulty = db.Column(db.String)
+    prompt_description = db.Column(db.String(2000000))
     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"))
     user_prompts = relationship('User', back_populates='prompts_assigned')
 
@@ -172,42 +168,52 @@ class Prompt(db.Model):
 prompt_filename = 'Leetcode-data - leetcode.csv'
 
 def populate_prompt_tb():
-
     with open(prompt_filename, 'r') as csvfile:
         datareader = csv.reader(csvfile)
         for row in datareader:
             split_data = row[0].split("--")
-            prompt_name_data, prompt_link_data, prompt_difficulty_data = split_data
-            populate_prompt = Prompt(prompt_name = prompt_name_data, prompt_link = prompt_link_data, prompt_difficulty = prompt_difficulty_data )
+            prompt_name_data, prompt_link_data, prompt_difficulty_data, prompt_description_data = split_data
+            populate_prompt = Prompt(prompt_name = prompt_name_data, prompt_link = prompt_link_data, prompt_difficulty = prompt_difficulty_data, prompt_description=prompt_description_data )
             db.session.add(populate_prompt)
             db.session.commit()
+
+comment_body = ['I am very curious if anyone was able to solve this prompt using Java! The solutions section did not provide much help for me',
+                'The solutions under the solutions part of Leetcode looked so elegant, my solution is not comparable, I was able to solve it though!',
+                'Gotta try again, could not solve in 30 minutes!',
+                'I was able to solve this problem within the timeframe!']
+
+def populate_comment_tb():
+    n = 9
+    while (n > 1):
+        populate_comments = Comment(body = random.choice(comment_body), user_id = random.randint(1,150), post_id = random.randint(1,10))
+        db.session.add(populate_comments)
+        db.session.commit()
+        n-= 1
+
 
 user_filename = 'User-data.csv'
 
 def populate_user_tb():
-
     with open(user_filename, 'r') as csvfile:
         datareader = csv.reader(csvfile)
+        folder_dir = "/Users/gedailey/src/hackbright-project/static/profile_pics"
+
         for row in datareader:
             split_data = row[0].split("--")
+            profile_pic = random.choice(os.listdir("/Users/gedailey/src/hackbright-project/static/profile_pics"))
 
             first_name_data, last_name_data, email_data, pwd_data, prompt_diff_data, primary_lang_data, timezone_data, prog_name_data, selected_timeslots_data = split_data
-
-            populate_user = User(first_name = first_name_data, last_name = last_name_data, email = email_data, password = pwd_data, prompt_difficulty_level=prompt_diff_data,primary_language=primary_lang_data, timezone_name= timezone_data )
-
+            populate_user = User(first_name = first_name_data, last_name = last_name_data, email = email_data, password = pwd_data, profile_picture= profile_pic, prompt_difficulty_level=prompt_diff_data,primary_language=primary_lang_data, timezone_name= timezone_data )
             db.session.add(populate_user)
             db.session.commit()
+
             user_id = User.query.filter_by(email=email_data).first().user_id
             get_timeslot_id = TimeSlot.query.filter_by(timeslot_name = selected_timeslots_data).one().timeslot_id
             populate_timeslot = UserTimeSlotMapping(user_id=user_id, timeslot_id=get_timeslot_id)
             get_prog_language_id = ProgrammingLanguage.query.filter_by(programming_language_name = prog_name_data).one().programming_language_id
             populate_prog_language = UserProgrammingLanguageMapping(user_id=user_id, programming_language_id=get_prog_language_id)
-
-
             db.session.add_all([populate_timeslot,populate_prog_language])
             db.session.commit()
-
-
 
 def populate_initial_db():
     programming_language_py = ProgrammingLanguage(programming_language_name = 'Python', programming_language_label = 'py')
@@ -227,9 +233,6 @@ def populate_initial_db():
                         prog_language_c, timeslot_7am, timeslot_10am,
                         timeslot_1pm,timeslot_4pm, timeslot_7pm, timeslot_10pm])
     db.session.commit()
-
-
-
 
 if __name__ == "__main__":
     from server import app
